440a9e5a6e2dbcef3671627f3c4fddbaa5545bb4
rditya <ra.raditya70@gmail.com>
2019-10-08 16:25:21 +0700

    merubah isi about dan menambahkan chart

---
+++ design/chart.js
@@ -0,0 +1,63 @@
+$( document ).ready(function() { // 6,32 5,38 2,34
+    $("#html").circliful({
+        animation: 1,
+        animationStep: 5,
+        foregroundBorderWidth: 15,
+        foregroundColor: "#4B253A",
+        backgroundBorderWidth: 15,
+        backgroundColor: "#D3B6C6",
+        percent: 90,
+        textSize: 28,
+        textStyle: 'font-size: 18px;',
+        textColor: '#4B253A',
+        text: 'HTML',
+        iconPosition: 'middle',
+        textY: '107'
+    });
+    $("#css").circliful({
+        animation: 1,
+        animationStep: 5,
+        foregroundBorderWidth: 15,
+        foregroundColor: "#45AEEA",
+        backgroundBorderWidth: 15,
+        backgroundColor: "#BEE3F7",
+        percent: 72,
+        textSize: 28,
+        textStyle: 'font-size: 18px;',
+        textColor: '#45AEEA',
+        text: 'CSS',
+        iconPosition: 'middle',
+        textY: '107'
+    });
+    $("#php7").circliful({
+        animation: 1,
+        animationStep: 5,
+        foregroundBorderWidth: 15,
+        foregroundColor: "#EFB917",
+        backgroundBorderWidth: 15,
+        backgroundColor: "#FCE6A4",
+        percent: 78,
+        textSize: 28,
+        textStyle: 'font-size: 18px;',
+        textColor: '#EFB917',
+        text: 'PHP7',
+        iconPosition: 'middle',
+        textY: '107'
+    });
+    $("#laravel").circliful({
+        animation: 1,
+        animationStep: 5,
+        foregroundBorderWidth: 15,
+        foregroundColor: "#D43A43",
+        backgroundBorderWidth: 15,
+        backgroundColor: "#F4BCBF",
+        percent: 85,
+        textSize: 28,
+        textStyle: 'font-size: 18px;',
+        textColor: '#D43A43',
+        text: 'Laravel',
+        iconPosition: 'middle',
+        textY: '107'
+    });
+    
+});
